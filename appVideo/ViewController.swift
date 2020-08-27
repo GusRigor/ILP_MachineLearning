7c4bde623675cb0bcef84204b0bda0fbac008c1c
Gustavo Rigor <gusrigor@icloud.com>
2020-08-27 19:05:17 -0300

    Initial Commit

---
+++ appVideo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  appVideo
+//
+//  Created by Gustavo Rigor on 27/08/20.
+//  Copyright © 2020 Gustavo Rigor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
